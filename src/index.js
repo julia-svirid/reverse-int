9bb84ec9fd7946956da8f818e2dbf1ad85e976e2
julia-svirid <yulia1903@gmail.com>
2020-03-06 21:38:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if (n<0){
+    n=-n;
+  }
+  n = String(n);
+  let arr = n.split('');
+  arr.reverse();
+  let str = arr.join('');
+  let result = Number(str);
+
+  return result;
 }
